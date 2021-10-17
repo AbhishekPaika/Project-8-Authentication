2d847ae6dfea00f86e7d19a65d2945ba17cddf9a
AbhishekPaika <abhishek.paika@gmail.com>
2021-10-18 04:54:12 +0530

    add passport for session and cookies

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5"); ///// Level 3 - Hashing //////
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// const bcrypt = require("bcrypt"); ///// Level 4 - bcrypt Hashing //////
+// const saltRounds = 10;
+// const md5 = require("md5"); ///// Level 3 - md5 Hashing //////
 // const encrypt = require("mongoose-encryption"); ///// Level 2 - Encryption //////
 
 const app = express();
@@ -12,6 +17,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/newUserDB', {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
@@ -19,18 +33,42 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 // const secret = process.env.SECRET; ///// Level 2 - Encryption //////
 // userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]}) ///// Level 2 - Encryption //////
 
 const User = new mongoose.model("User", userSchema);
 
-//////////////////////////////// ROOT ROUTE /////////////////////////////////////
+passport.use(User.createStrategy());
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//////////////////////////////// ROOT ROUTE /////////////////////////////////////
 app.route("/")
 
 .get(function(req, res){
   res.render("home");
 })
+
+//////////////////////////////// SECRETS ROUTE /////////////////////////////////////
+app.route("/secrets")
+
+.get(function(req, res){
+    if(req.isAuthenticated()){
+      res.render("secrets");
+    }else{
+      res.redirect("/login");
+    }
+});
+
+//////////////////////////////// LOG-OUT ROUTE /////////////////////////////////////
+app.route("/logout")
+
+.get(function(req, res){
+  req.logout();
+  res.redirect("/");
+});
 
 //////////////////////////////// REGISTER ROUTE /////////////////////////////////////
 
@@ -43,17 +81,16 @@ app.route("/register")
 .post(function(req, res){
 
   const email = req.body.username;
-  const password = md5(req.body.password); ///// Level 3 - Hashing //////
+  const password = req.body.password;
 
-  const newUser = new User({
-    email: email,
-    password: password
-  });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets")
+  User.register({username: email}, password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
     }else{
-      res.render(err);
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
 });
@@ -69,23 +106,23 @@ app.route("/login")
 .post(function(req, res){
 
   const username = req.body.username;
-  const password = md5(req.body.password); ///// Level 3 - Hashing //////
+  const password = req.body.password;
 
-  User.findOne({email:username}, function(err, foundUser){
+  const user = new User({
+    username: username,
+    password: password
+  });
 
-  if(foundUser){
-    if(foundUser.password === password){
-      res.render("secrets");
-    }
-  }else{
-      res.render(err);
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
 });
-
-
-
-
 
 
 
